a66c845d5d0f35c59e20f8523337a61a8a45a7d0
unknown <sara.reidy.21@gmail.com>
2020-07-29 20:51:32 -0400

    cleared warnings

--- src/grid/index.js
+++ src/grid/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import {GridBody, Cell} from './styles'
 
 const Grid = ({gridSize, viewingArray, handleClick, color}) => {
